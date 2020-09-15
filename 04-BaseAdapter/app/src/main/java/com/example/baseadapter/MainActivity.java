d180f996291169440bfa341c439b952950f75742
PromiseAll <2238480780@qq.com>
2020-09-15 23:29:30 +0800

    添加删除功能

--- 04-BaseAdapter/app/src/main/java/com/example/baseadapter/MainActivity.java
+++ 04-BaseAdapter/app/src/main/java/com/example/baseadapter/MainActivity.java
@@ -12,25 +12,27 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ListView list;
     MyBaseAdapter adapter;
-    ArrayList<String> data =  new ArrayList<String>();
+    ArrayList<String> data = new ArrayList<String>();
+    int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
         this.list = this.findViewById(R.id.listView);
-        this.adapter = new MyBaseAdapter(this.getApplicationContext(),this.data);
+        this.adapter = new MyBaseAdapter(this.getApplicationContext(), this.data);
         this.list.setAdapter(adapter);
         this.addEvent();
     }
 
-    public void addEvent(){
+    public void addEvent() {
         Button addBtn = this.findViewById(R.id.button);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.this.data.add("添加数据");
+                MainActivity.this.data.add(String.valueOf(MainActivity.this.num));
                 MainActivity.this.adapter.notifyDataSetChanged();
+                MainActivity.this.num++;
             }
         });
     }
--- 04-BaseAdapter/app/src/main/java/com/example/baseadapter/MyBaseAdapter.java
+++ 04-BaseAdapter/app/src/main/java/com/example/baseadapter/MyBaseAdapter.java
@@ -1,11 +1,12 @@
 package com.example.baseadapter;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,38 +18,43 @@ public class MyBaseAdapter extends BaseAdapter {
     private ArrayList<String> data;
 
     public MyBaseAdapter(Context context, ArrayList<String> data) {
-        this.mContext =  context;
-        this.inflater = LayoutInflater.from(context);
-        this.data = data;
+        this.mContext = context;  //定义上下文,由MainActivity传入
+        this.inflater = LayoutInflater.from(context); //获取布局管理器
+        this.data = data; //获取传入的数据
     }
 
+    // 获取一个有几个item
     @Override
     public int getCount() {
         return this.data.size();
     }
 
+    // 获取当前屏幕渲染item位置对应data数据
     @Override
     public Object getItem(int position) {
-        return null;
+        return this.data.get(position);
     }
 
+    // 获取当前屏幕渲染item位置
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+        final ViewHolder holder;
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.list_item, null);
             holder = new ViewHolder();
             holder.text = convertView.findViewById(R.id.text);
+            holder.delete_btn = convertView.findViewById(R.id.delete);
+            holder.setEvent();
             convertView.setTag(holder);
-            convertView.setOnClickListener(new View.OnClickListener(){
+            convertView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MyBaseAdapter.this.mContext, data.get(position),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MyBaseAdapter.this.mContext, holder.text.getText(), Toast.LENGTH_SHORT).show();
                 }
             });
         } else {
@@ -58,7 +64,23 @@ public class MyBaseAdapter extends BaseAdapter {
         return convertView;
     }
 
-    static class ViewHolder {
+    class ViewHolder implements View.OnClickListener {
+        // 实现View.OnClickListener接口
         TextView text;
+        Button delete_btn;
+
+        public void setEvent() {
+            // 设置点击事件,this 传入当前实现接口类实例
+            this.delete_btn.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.delete:
+                    MyBaseAdapter.this.data.remove(this.text.getText());
+                    MyBaseAdapter.this.notifyDataSetChanged();
+            }
+        }
     }
 }
